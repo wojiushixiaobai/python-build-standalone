wojiushixiaobai/python-build-standalone

commit e0ebbc70ec741cc5ee7c45046046e13b414e1ea1

Retry on transient failures during artifact upload (#486)
Attempting to fix transient release failures, e.g,
https://github.com/astral-sh/python-build-standalone/actions/runs/12637445294/job/35213894154

Cargo.toml
@@ -26,6 +26,8 @@ pdb = "0.8.0"
 pep440_rs = "0.6.6"
 rayon = "1.8.1"
 reqwest = { version = "0.11.24", features = ["rustls", "stream"] }
+reqwest-middleware = "0.4.0"
+reqwest-retry = "0.7.0"
 scroll = "0.12.0"
 semver = "1.0.22"
 serde = { version = "1.0.197", features = ["derive"] }

src/github.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::str::FromStr;
-
 use {
     crate::release::{
         bootstrap_llvm, produce_install_only, produce_install_only_stripped, RELEASE_TRIPLES,
@@ -18,15 +16,38 @@ use {
         Octocrab, OctocrabBuilder,
     },
     rayon::prelude::*,
+    reqwest::StatusCode,
+    reqwest_middleware::{reqwest, ClientBuilder, ClientWithMiddleware},
+    reqwest_retry::{
+        default_on_request_failure, policies::ExponentialBackoff, RetryTransientMiddleware,
+        Retryable, RetryableStrategy,
+    },
     sha2::{Digest, Sha256},
     std::{
         collections::{BTreeMap, BTreeSet, HashMap},
         io::Read,
         path::PathBuf,
+        str::FromStr,
     },
     url::Url,
     zip::ZipArchive,
 };
+
+/// A retry strategy for GitHub uploads.
+struct GitHubUploadRetryStrategy;
+impl RetryableStrategy for GitHubUploadRetryStrategy {
+    fn handle(
+        &self,
+        res: &std::result::Result<reqwest::Response, reqwest_middleware::Error>,
+    ) -> Option<Retryable> {
+        match res {
+            // Retry on 403s, these indicate a transient failure.
+            Ok(success) if success.status() == StatusCode::FORBIDDEN => Some(Retryable::Transient),
+            Ok(_) => None,
+            Err(error) => default_on_request_failure(error),
+        }
+    }
+}
 
 async fn fetch_artifact(
     client: &Octocrab,
@@ -45,6 +66,7 @@ async fn fetch_artifact(
 }
 
 async fn upload_release_artifact(
+    client: &ClientWithMiddleware,
     auth_token: String,
     release: &Release,
     filename: String,
@@ -74,8 +96,7 @@ async fn upload_release_artifact(
     // Octocrab doesn't yet support release artifact upload. And the low-level HTTP API
     // forces the use of strings on us. So we have to make our own HTTP client.
 
-    let response = reqwest::Client::builder()
-        .build()?
+    let response = client
         .put(url)
         .header("Authorization", format!("Bearer {auth_token}"))
         .header("Content-Length", data.len())
@@ -451,42 +472,54 @@ pub async fn command_upload_release_distributions(args: &ArgMatches) -> Result<(
 
     let mut digests = BTreeMap::new();
 
-    let mut fs = vec![];
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(5);
+    let raw_client = ClientBuilder::new(reqwest_middleware::reqwest::Client::new())
+        .with(RetryTransientMiddleware::new_with_policy_and_strategy(
+            retry_policy,
+            GitHubUploadRetryStrategy,
+        ))
+        .build();
 
-    for (source, dest) in wanted_filenames {
-        if !filenames.contains(&source) {
-            continue;
+    {
+        let mut fs = vec![];
+
+        for (source, dest) in wanted_filenames {
+            if !filenames.contains(&source) {
+                continue;
+            }
+
+            let file_data = Bytes::copy_from_slice(&std::fs::read(dist_dir.join(&source))?);
+
+            let mut digest = Sha256::new();
+            digest.update(&file_data);
+
+            let digest = hex::encode(digest.finalize());
+
+            digests.insert(dest.clone(), digest.clone());
+
+            fs.push(upload_release_artifact(
+                &raw_client,
+                token.clone(),
+                &release,
+                dest.clone(),
+                file_data,
+                dry_run,
+            ));
+            fs.push(upload_release_artifact(
+                &raw_client,
+                token.clone(),
+                &release,
+                format!("{}.sha256", dest),
+                Bytes::copy_from_slice(format!("{}\n", digest).as_bytes()),
+                dry_run,
+            ));
         }
 
-        let file_data = Bytes::copy_from_slice(&std::fs::read(dist_dir.join(&source))?);
+        let mut buffered = futures::stream::iter(fs).buffer_unordered(16);
 
-        let mut digest = Sha256::new();
-        digest.update(&file_data);
-
-        let digest = hex::encode(digest.finalize());
-
-        digests.insert(dest.clone(), digest.clone());
-
-        fs.push(upload_release_artifact(
-            token.clone(),
-            &release,
-            dest.clone(),
-            file_data,
-            dry_run,
-        ));
-        fs.push(upload_release_artifact(
-            token.clone(),
-            &release,
-            format!("{}.sha256", dest),
-            Bytes::copy_from_slice(format!("{}\n", digest).as_bytes()),
-            dry_run,
-        ));
-    }
-
-    let mut buffered = futures::stream::iter(fs).buffer_unordered(16);
-
-    while let Some(res) = buffered.next().await {
-        res?;
+        while let Some(res) = buffered.next().await {
+            res?;
+        }
     }
 
     let shasums = digests
@@ -498,6 +531,7 @@ pub async fn command_upload_release_distributions(args: &ArgMatches) -> Result<(
     std::fs::write(dist_dir.join("SHA256SUMS"), shasums.as_bytes())?;
 
     upload_release_artifact(
+        &raw_client,
         token.clone(),
         &release,
         "SHA256SUMS".to_string(),

commit fc29abe434cdbf48426bc1c1270232b898f2f5f2

Upgrade `reqwest` to 0.12 (#500)
Fixes artifact upload failures introduced by conflicting versions in
#486

Cargo.toml
@@ -25,7 +25,7 @@ once_cell = "1.19.0"
 pdb = "0.8.0"
 pep440_rs = "0.6.6"
 rayon = "1.8.1"
-reqwest = { version = "0.11.24", features = ["rustls", "stream"] }
+reqwest = { version = "0.12.12", features = ["rustls-tls-webpki-roots", "stream"] }
 reqwest-middleware = "0.4.0"
 reqwest-retry = "0.7.0"
 scroll = "0.12.0"

src/github.rs
@@ -16,8 +16,8 @@ use {
         Octocrab, OctocrabBuilder,
     },
     rayon::prelude::*,
-    reqwest::StatusCode,
-    reqwest_middleware::{reqwest, ClientBuilder, ClientWithMiddleware},
+    reqwest::{Client, StatusCode},
+    reqwest_middleware::{self, ClientWithMiddleware},
     reqwest_retry::{
         default_on_request_failure, policies::ExponentialBackoff, RetryTransientMiddleware,
         Retryable, RetryableStrategy,
@@ -473,7 +473,7 @@ pub async fn command_upload_release_distributions(args: &ArgMatches) -> Result<(
     let mut digests = BTreeMap::new();
 
     let retry_policy = ExponentialBackoff::builder().build_with_max_retries(5);
-    let raw_client = ClientBuilder::new(reqwest_middleware::reqwest::Client::new())
+    let raw_client = reqwest_middleware::ClientBuilder::new(Client::new())
         .with(RetryTransientMiddleware::new_with_policy_and_strategy(
             retry_policy,
             GitHubUploadRetryStrategy,