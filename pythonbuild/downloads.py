19bdff5dc02b8c76af7c55072d156a58bef02d5f
Zanie Blue <contact@zanie.dev>
2025-03-17 13:24:48 -0500

    Use a mirror for bzip2 (#567)
    A more targeted solution to
    
    - #566 
    - #565

--- pythonbuild/downloads.py
+++ pythonbuild/downloads.py
@@ -26,7 +26,9 @@ DOWNLOADS = {
         "version": "2.43",
     },
     "bzip2": {
-        "url": "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz",
+        # Mirror of `https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz` due to
+        # rate limiting
+        "url": "https://astral-sh.github.io/mirror/files/bzip2-1.0.8.tar.gz",
         "size": 810029,
         "sha256": "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269",
         "version": "1.0.8",
