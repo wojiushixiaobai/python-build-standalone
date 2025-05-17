d8ef90ad326e0e8b7074ba097df5a414288ff3c6
Gregory Szorc <gregory.szorc@gmail.com>
2025-05-16 19:18:59 -0700

    xorgproto 2019.1 -> 2024.1
    I think deletion of the legacy proto packages a few commits ago fixed up
    the build failures the deleted comment alluded to.

--- pythonbuild/downloads.py
+++ pythonbuild/downloads.py
@@ -383,13 +383,11 @@ DOWNLOADS = {
         "sha256": "2c1bacd2110f4799f74de6ebb714b94cf6f80fb112316b1219480fd22562148c",
         "version": "1.17.0",
     },
-    # Newer versions from at least 2023 have build failures for reasons we haven't
-    # fully investigated.
     "xorgproto": {
-        "url": "https://www.x.org/archive/individual/proto/xorgproto-2019.1.tar.gz",
-        "size": 1119813,
-        "sha256": "38ad1d8316515785d53c5162b4b7022918e03c11d72a5bd9df0a176607f42bca",
-        "version": "2019.1",
+        "url": "https://www.x.org/archive/individual/proto/xorgproto-2024.1.tar.gz",
+        "size": 1115486,
+        "sha256": "4f6b9b4faf91e5df8265b71843a91fc73dc895be6210c84117a996545df296ce",
+        "version": "2024.1",
     },
     "xtrans": {
         "url": "https://www.x.org/archive/individual/lib/xtrans-1.6.0.tar.gz",
