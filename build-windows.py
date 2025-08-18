wojiushixiaobai/python-build-standalone

commit ce0f50af1b88bd897fcab922d87a204656a879bc

Fix download statistics (#706)
`just download-stats*` are failing with an HTTP 504 Gateway Timeout on
GitHub's servers.

We can work around this by reducing the requested number of releases per
page.

As part of this, we adopt a 3rd party package for querying the GitHub
API because I was too lazy to implement pagination manually. Also, since
pagination is enabled, the counts reported by these commands are now
accurate and don't implicitly truncate at the most recent 10 releases.

Co-authored-by: Zanie Blue <contact@zanie.dev>

pythonbuild/utils.py
@@ -23,6 +23,7 @@ import urllib.error
 import urllib.request
 import zipfile
 
+import github
 import yaml
 import zstandard
 
@@ -653,21 +654,20 @@ def validate_python_json(info, extension_modules):
 
 
 def release_download_statistics(mode="by_asset"):
-    with urllib.request.urlopen(
-        "https://api.github.com/repos/astral-sh/python-build-standalone/releases"
-    ) as fh:
-        data = json.load(fh)
-
     by_tag = collections.Counter()
     by_build = collections.Counter()
     by_build_install_only = collections.Counter()
 
-    for release in data:
-        tag = release["tag_name"]
+    # Default paging settings time out. Reduce page size as a workaround.
+    gh = github.Github(per_page=5)
 
-        for asset in release["assets"]:
-            name = asset["name"]
-            count = asset["download_count"]
+    repo = gh.get_repo("astral-sh/python-build-standalone")
+    for release in repo.get_releases():
+        tag = release.tag_name
+
+        for asset in release.assets:
+            name = asset.name
+            count = asset.download_count
 
             by_tag[tag] += count
 

requirements.in
@@ -1,6 +1,7 @@
 docker
 jinja2
 jsonschema
+PyGithub
 PyYAML
 # Undeclared dependency in docker 5.0 package.
 six

requirements.dev.in
@@ -2,6 +2,7 @@
 
 ruff
 mypy
+PyGithub
 types-jsonschema
 types-PyYAML
 types-jinja2

commit 01aa80641edfca8efa1a8f7c3b9bb0405bde1512

Exclude pygithub on arm64 Windows (#769)
Addressing the environment bootstrap regression from #706 — we can't
build `cryptography` and `pynacl` from source and we don't need download
statistics in CI.

build-windows.py
@@ -5,6 +5,7 @@
 
 import os
 import pathlib
+import platform
 import subprocess
 import sys
 import venv
@@ -15,7 +16,8 @@ DIST = ROOT / "dist"
 VENV = BUILD / "venv"
 PIP = VENV / "Scripts" / "pip.exe"
 PYTHON = VENV / "Scripts" / "python.exe"
-REQUIREMENTS = ROOT / "requirements.win.txt"
+ARCH = "-arm64" if platform.machine() == "ARM64" else ""
+REQUIREMENTS = ROOT / f"requirements.win{ARCH}.txt"
 WINDOWS_DIR = ROOT / "cpython-windows"
 
 

pythonbuild/utils.py
@@ -23,7 +23,6 @@ import urllib.error
 import urllib.request
 import zipfile
 
-import github
 import yaml
 import zstandard
 
@@ -654,6 +653,8 @@ def validate_python_json(info, extension_modules):
 
 
 def release_download_statistics(mode="by_asset"):
+    import github
+
     by_tag = collections.Counter()
     by_build = collections.Counter()
     by_build_install_only = collections.Counter()

requirements.in
@@ -1,7 +1,9 @@
 docker
 jinja2
 jsonschema
-PyGithub
+# This does not built on the aarch64-windows runners yet, and is only needed for
+# development so we'll just skip it for now.
+PyGithub; sys_platform != "win32" or platform_machine != "aarch64"
 PyYAML
 # Undeclared dependency in docker 5.0 package.
 six